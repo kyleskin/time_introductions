498d9451964debb6e142bcab716b6593274c4c38
Kyle Skinner <kyleskin@gmail.com>
2016-03-15 12:22:23 -0600

    Time
    * Created time/now route
    
    [#115708251]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get 'time/now' => 'time#now'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
